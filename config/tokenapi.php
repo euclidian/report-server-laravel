68ec44e1a60b8519323e86e59f528e345926aaeb
ahmadhasya <52899635+ahmadhasya@users.noreply.github.com>
2019-07-17 11:02:47 +0700

    Travis File (#5)
    * Travis File
    
    * Ganti Repo
    
    * Update lock
    
    * salah require
    
    * menambah folder json
    
    * Hapus storage json
    
    * Debugging
    
    * Ubah client secret
    
    * Done

--- config/tokenapi.php
+++ config/tokenapi.php
@@ -6,8 +6,8 @@ return [
     "upload_url" 	=> env('TOKEN_UPLOAD_URL', "https://media-stg.asmatende.asmat.app/api/image/upload"),
     "download_url" 	=> env('TOKEN_DOWNLOAD_URL', "https://media-stg.asmatende.asmat.app"),
     "grant_type" 	=> env('TOKEN_GRANT_TYPE', "client_credentials"),
-    "client_id"	 	=> env('TOKEN_CLIENT_ID', 3),
-    "client_secret"	=> env('TOKEN_CLIENT_SECRET', "rxwKRNXi1sGJycdPhOM8SdEDI7nJAIj9PXybmTAM"),
+    "client_id"	 	=> env('TOKEN_CLIENT_ID', 6),
+    "client_secret"	=> env('TOKEN_CLIENT_SECRET', "gDlzG4dFRDlJfnw2bUxgxfOx7HvRZharZzr4oeHB"),
     "username"		=> env('TOKEN_USERNAME', "a@a.com"),
     "password"		=> env('TOKEN_PASSWORD', "123456")
 ];
