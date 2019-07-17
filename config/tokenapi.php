ba10dbdbf6e74f717665b4c5037042c84535017f
Ahmad Hasya <hasyazidane2@gmail.com>
2019-07-17 13:20:44 +0700

    Membersihkan

--- config/tokenapi.php
+++ config/tokenapi.php
@@ -2,10 +2,10 @@
 
 return [
     "group" 	 	=> env('TOKEN_GROUP', "PrintAPI"),
-    "url" 		 	=> env('TOKEN_URL', "https://media-stg.asmatende.asmat.app/oauth/token"),
-    "upload_url" 	=> env('TOKEN_UPLOAD_URL', "https://media-stg.asmatende.asmat.app/api/image/upload"),
-    "download_url" 	=> env('TOKEN_DOWNLOAD_URL', "https://media-stg.asmatende.asmat.app"),
-    "grant_type" 	=> env('TOKEN_GRANT_TYPE', "client_credentials"),
+    "url" 		 	=> env('TOKEN_URL'),
+    "upload_url" 	=> env('TOKEN_UPLOAD_URL'),
+    "download_url" 	=> env('TOKEN_DOWNLOAD_URL'),
+    "grant_type" 	=> env('TOKEN_GRANT_TYPE'),
     "client_id"	 	=> env('TOKEN_CLIENT_ID'),
     "client_secret"	=> env('TOKEN_CLIENT_SECRET'),
     "username"		=> env('TOKEN_USERNAME'),
