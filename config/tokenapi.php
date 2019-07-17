7c37e49354e3b096dbb237dbdb1ed81d3fe23c5d
Ahmad Hasya <hasyazidane2@gmail.com>
2019-07-17 11:39:38 +0700

    Enkripsi token

--- config/tokenapi.php
+++ config/tokenapi.php
@@ -6,8 +6,8 @@ return [
     "upload_url" 	=> env('TOKEN_UPLOAD_URL', "https://media-stg.asmatende.asmat.app/api/image/upload"),
     "download_url" 	=> env('TOKEN_DOWNLOAD_URL', "https://media-stg.asmatende.asmat.app"),
     "grant_type" 	=> env('TOKEN_GRANT_TYPE', "client_credentials"),
-    "client_id"	 	=> env('TOKEN_CLIENT_ID', 6),
-    "client_secret"	=> env('TOKEN_CLIENT_SECRET', "gDlzG4dFRDlJfnw2bUxgxfOx7HvRZharZzr4oeHB"),
-    "username"		=> env('TOKEN_USERNAME', "a@a.com"),
-    "password"		=> env('TOKEN_PASSWORD', "123456")
+    "client_id"	 	=> env('TOKEN_CLIENT_ID'),
+    "client_secret"	=> env('TOKEN_CLIENT_SECRET'),
+    "username"		=> env('TOKEN_USERNAME'),
+    "password"		=> env('TOKEN_PASSWORD')
 ];
