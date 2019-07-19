70cdb2b5a84d55469a15939e7d530b83b161cda3
Ahmad Hasya <hasyazidane2@gmail.com>
2019-07-19 17:15:16 +0700

    Add unit test TemplateComponent

--- tests/Javascript/TemplateComponent.test.js
+++ tests/Javascript/TemplateComponent.test.js
@@ -1,17 +1,18 @@
-
 import TemplateComponent from "../../resources/js/components/TemplateComponent";
-import {mount} from '@vue/test-utils'
-import expect from 'expect'
 
+import { mount } from "@vue/test-utils";
+import expect from "expect";
+// import Vuetify from 'vuetify';
+// var Vue = require("vue");
+// Vue.use(Vuetify);
 describe("TemplateComponent", () => {
-  let component
+    let component;
 
-  beforeEach(() => {
-    component = mount(TemplateComponent)
-  })
+    beforeEach(() => {
+        component = mount(TemplateComponent);
+    });
 
-  it('Contains TemplateComponent', () => {
-    expect(component.html()).toContain('TemplateComponent')
-  })
-
-})
+    it("Contains TemplateComponent", () => {
+        expect(component.html()).toContain(TemplateComponent);
+    });
+});
--- tests/Javascript/setup.js
+++ tests/Javascript/setup.js
@@ -1,1 +1,2 @@
 require('jsdom-global')()
+axios=require('axios')
